26d4a3edf1d5e2a9f543dbdfd2f293d4aed556ce
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2023-04-23 16:40:46 +0545

    feat: Without package implemented for Usecase 2

--- src/Usecase2/ReadMe.md
+++ src/Usecase2/ReadMe.md
@@ -1,1 +1,45 @@
 # Date conversion between different formats
+
+[👈 Back](../../ReadMe.md)
+
+💻 **How to check:** Pointing yourself at root of the project run `yarn uc2` and you will see the code execution of all the formats with and without library.
+
+Here are some of the commonly used formats.
+
+1. **ISO 8601 format:** `YYYY-MM-DDTHH:MM:SSZ` -> `2023-04-22T09:30:00Z`
+2. **American format:** `MM/DD/YYYY` or `MM/DD/YY` -> `04/22/2023` or `04/22/23`
+3. **European format:** `DD/MM/YYYY` or `DD/MM/YY` -> `22/04/2023` or `22/04/23`
+4. **Written-out format:** `Month DD, YYYY` -> `April 22, 2023`
+5. **Unix timestamp:** `Number of seconds since January 1, 1970, at 00:00:00 UTC` -> `1,768,167,600`
+6. **24-hour clock:** `HH:MM:SS` -> `09:30:00`
+7. **RFC 2822 format:** `Day, DD Mon YYYY HH:MM:SS +ZZZZ` -> `Fri, 22 Apr 2023 09:30:00 +0000`
+8. **Long date format:** `Day, Month DD, YYYY` -> `Friday, April 22, 2023`
+
+Formats based on nationalities.
+
+1. **United States:** `MM/DD/YYYY` or `MM/DD/YY` (for example, 04/22/2023 or 04/22/23)
+The United States also uses a 12-hour clock format with AM and PM, such as 9:30 AM or 2:45 PM.
+
+2. **United Kingdom:** `DD/MM/YYYY` or `DD/MM/YY` (for example, 22/04/2023 or 22/04/23)
+The United Kingdom typically uses a 24-hour clock format, such as 09:30 or 14:45.
+
+3. **Canada:** `MM/DD/YYYY` or `DD/MM/YYYY` (both formats are used depending on the region)
+Canada also typically uses a 12-hour clock format with AM and PM, such as 9:30 AM or 2:45 PM.
+
+4. **Australia:** `DD/MM/YYYY` or `DD/MM/YY` (for example, 22/04/2023 or 22/04/23)
+Australia typically uses a 24-hour clock format, such as 09:30 or 14:45.
+
+5. **Japan:** `YYYY年MM月DD日` (for example, 2023年04月22日)
+Japan typically uses a 24-hour clock format, such as 09:30 or 14:45.
+
+6. **China:** `YYYY年MM月DD日` (for example, 2023年04月22日)
+China typically uses a 24-hour clock format, such as 09:30 or 14:45.
+
+7. **Germany:** `DD.MM.YYYY` (for example, 22.04.2023)
+Germany typically uses a 24-hour clock format, such as 09:30 or 14:45.
+
+## References
+
+1. [RFC3339 standards docs](https://www.rfc-editor.org/rfc/rfc3339)
+2. [ISO 8601 standards docs](https://www.w3.org/TR/NOTE-datetime)
+3. [MDN - toLocaleDateString](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString)
---
+++ src/Usecase2/index.ts
@@ -0,0 +1,392 @@
+import { dateFromBackend, execUsecase } from "../utils";
+
+const Usecase2 = {
+    withoutPackage: {
+        // Common formats
+        commonFormats: {
+            ISO8601: () => {
+                console.log("\tWithout Package - Common Format - [ISO8601]\n");
+                const time = new Date(dateFromBackend).toISOString();
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to ISO8601: ",
+                    time
+                );
+                return time;
+            },
+            American: () => {
+                console.log(
+                    "\n\tWithout Package - American Format - [MM/DD/YYYY or MM/DD/YY]\n"
+                );
+                const format1 = new Date(dateFromBackend).toLocaleDateString(
+                    "en-US"
+                );
+                const format2 = new Date(dateFromBackend).toLocaleDateString(
+                    "en-US",
+                    {
+                        month: "2-digit",
+                        day: "2-digit",
+                        year: "2-digit",
+                    }
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to American Format: ",
+                    format1,
+                    " or ",
+                    format2
+                );
+                return {
+                    format1,
+                    format2,
+                };
+            },
+            European: () => {
+                console.log(
+                    "\n\tWithout Package - European Format - [DD/MM/YYYY or DD/MM/YY]\n"
+                );
+                const format1 = new Date(dateFromBackend).toLocaleDateString(
+                    "CA"
+                );
+                const format2 = new Date(dateFromBackend).toLocaleDateString(
+                    "CA",
+                    {
+                        month: "2-digit",
+                        day: "2-digit",
+                        year: "2-digit",
+                    }
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to European Format: ",
+                    format1,
+                    " or ",
+                    format2
+                );
+                return {
+                    format1,
+                    format2,
+                };
+            },
+            WrittenOut: () => {
+                console.log(
+                    "\n\tWithout Package - Written Out Format - [Month DD, YYYY]\n"
+                );
+                const result = new Date(dateFromBackend).toLocaleDateString(
+                    undefined,
+                    {
+                        month: "long",
+                        day: "2-digit",
+                        year: "numeric",
+                    }
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to Written Out Format: ",
+                    result
+                );
+                return result;
+            },
+            UnixTimestamp: () => {
+                console.log(
+                    "\n\tWithout Package - Common Format - [UnixTimestamp]\n"
+                );
+                const time = Date.parse(dateFromBackend);
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to UnixTimestamp: ",
+                    time
+                );
+                return time;
+            },
+            "24Hours": () => {
+                console.log(
+                    "\n\tWithout Package - 24Hours Format - [HH:MM:SS]\n"
+                );
+                const time = new Date(dateFromBackend).toLocaleTimeString(
+                    undefined,
+                    {
+                        hour12: false,
+                    }
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to 24Hours format: ",
+                    time
+                );
+                return time;
+            },
+            RFC2822: () => {
+                console.log(
+                    "\n\tWithout Package - RFC2822 Format - [Day, DD Mon YYYY HH:MM:SS +ZZZZ]\n"
+                );
+                const inpDate = new Date(dateFromBackend);
+                const timeFormat = ` ${inpDate.toLocaleDateString("en-GB", {
+                    month: "short",
+                    day: "2-digit",
+                    weekday: "short",
+                })} ${inpDate.getFullYear()} ${inpDate.toLocaleTimeString(
+                    "en-GB",
+                    {
+                        hour12: false,
+                    }
+                )} ${inpDate.getTimezoneOffset()}`;
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to RFC2822 format: ",
+                    timeFormat
+                );
+                return timeFormat;
+            },
+            LongDate: () => {
+                console.log(
+                    "\n\tWithout Package - Long Date Format - [Day, Month DD, YYYY]\n"
+                );
+                const time = new Date(dateFromBackend).toLocaleDateString(
+                    undefined,
+                    {
+                        day: "2-digit",
+                        weekday: "short",
+                        month: "long",
+                        year: "numeric",
+                    }
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to RFC2822 format: ",
+                    time
+                );
+                return time;
+            },
+        },
+        // Based on nationalities
+        nationalities: {
+            UnitedStates: () => {
+                console.log(
+                    "\n\tWithout Package - United States Format - [MM/DD/YYYY or MM/DD/YY]\n"
+                );
+                const format1 = new Date(dateFromBackend).toLocaleDateString(
+                    "en-US"
+                );
+                const format2 = new Date(dateFromBackend).toLocaleDateString(
+                    "en-US",
+                    {
+                        month: "2-digit",
+                        day: "2-digit",
+                        year: "2-digit",
+                    }
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to United States Format: ",
+                    format1,
+                    " or ",
+                    format2
+                );
+                return {
+                    format1,
+                    format2,
+                };
+            },
+            UnitedKingdom: () => {
+                console.log(
+                    "\n\tWithout Package - United Kingdom Format - [DD/MM/YYYY or DD/MM/YY]\n"
+                );
+                const format1 = new Date(dateFromBackend).toLocaleDateString(
+                    "en-GB"
+                );
+                const format2 = new Date(dateFromBackend).toLocaleDateString(
+                    "en-GB",
+                    {
+                        month: "2-digit",
+                        day: "2-digit",
+                        year: "2-digit",
+                    }
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to United Kingdom Format: ",
+                    format1,
+                    " or ",
+                    format2
+                );
+                return {
+                    format1,
+                    format2,
+                };
+            },
+            Canada: () => {
+                console.log(
+                    "\n\tWithout Package - Canada Format - [MM/DD/YYYY or DD/MM/YYYY]\n"
+                );
+                const format1 = new Date(dateFromBackend).toLocaleDateString(
+                    "en"
+                );
+                const format2 = new Date(dateFromBackend).toLocaleDateString(
+                    "CA",
+                    {
+                        month: "2-digit",
+                        day: "2-digit",
+                        year: "2-digit",
+                    }
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to Canada Format: ",
+                    format1,
+                    " or ",
+                    format2
+                );
+                return {
+                    format1,
+                    format2,
+                };
+            },
+            Australia: () => {
+                console.log(
+                    "\n\tWithout Package - Australia Format - [DD/MM/YYYY or DD/MM/YY]\n"
+                );
+                const format1 = new Date(dateFromBackend).toLocaleDateString(
+                    "AU"
+                );
+                const format2 = new Date(dateFromBackend).toLocaleDateString(
+                    "AU",
+                    {
+                        month: "2-digit",
+                        day: "2-digit",
+                        year: "2-digit",
+                    }
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to Australia Format: ",
+                    format1,
+                    " or ",
+                    format2
+                );
+                return {
+                    format1,
+                    format2,
+                };
+            },
+            Japan: () => {
+                console.log(
+                    "\n\tWithout Package - Japan Format - [YYYY年MM月DD日]\n"
+                );
+                const time = new Date(dateFromBackend).toLocaleDateString(
+                    "ja-JP",
+                    {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                        weekday: "long",
+                        hour12: false,
+                        hour: "2-digit",
+                        minute: "2-digit",
+                    }
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to Japan Format: ",
+                    time
+                );
+                return {
+                    time,
+                };
+            },
+            China: () => {
+                console.log(
+                    "\n\tWithout Package - China Format - [YYYY年MM月DD日]\n"
+                );
+                const time = new Date(dateFromBackend).toLocaleDateString(
+                    "zh-CH",
+                    {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                        weekday: "long",
+                        hour12: false,
+                        hour: "2-digit",
+                        minute: "2-digit",
+                    }
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to China Format: ",
+                    time
+                );
+                return {
+                    time,
+                };
+            },
+            Germany: () => {
+                console.log(
+                    "\n\tWithout Package - Germany Format - [DD.MM.YYYY]\n"
+                );
+                const time = new Date(dateFromBackend).toLocaleDateString(
+                    "de-DE",
+                    {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                        weekday: "long",
+                        hour12: false,
+                        hour: "numeric",
+                        minute: "numeric",
+                        timeZone: "Europe/Berlin",
+                    }
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to Germany Format: ",
+                    time
+                );
+                return {
+                    time,
+                };
+            },
+        },
+    },
+    withPackage: {
+        // TODO: Common formats
+        commonFormats: {
+            ISO8601: () => {},
+            American: () => {},
+            European: () => {},
+            WrittenOut: () => {},
+            UnixTimestamp: () => {},
+            "24Hours": () => {},
+            RFC2822: () => {},
+            LongDate: () => {},
+        },
+        // TODO: Based on nationalities
+        nationalities: {
+            UnitedStates: () => {},
+            UnitedKingdom: () => {},
+            Canada: () => {},
+            Australia: () => {},
+            Japan: () => {},
+            China: () => {},
+            Germany: () => {},
+        },
+    },
+};
+
+console.log("Begin - Usecase 2: Date conversion between different formats\n");
+execUsecase(Usecase2);
+console.log("\nEnd - Usecase 2: Date conversion between different formats");
