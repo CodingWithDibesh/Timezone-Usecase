b05f24907e20950ccb0d92871b497be726d2cd01
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2023-04-23 17:20:03 +0545

    feat: Usecase 2 completed

--- src/Usecase2/index.ts
+++ src/Usecase2/index.ts
@@ -1,4 +1,10 @@
+import dayjs from "dayjs";
 import { dateFromBackend, execUsecase } from "../utils";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const Usecase2 = {
     withoutPackage: {
@@ -341,13 +347,13 @@ const Usecase2 = {
                     "de-DE",
                     {
                         day: "numeric",
-                        month: "long",
+                        month: "numeric",
                         year: "numeric",
-                        weekday: "long",
+                        // weekday: "long",
                         hour12: false,
-                        hour: "numeric",
-                        minute: "numeric",
-                        timeZone: "Europe/Berlin",
+                        // hour: "numeric",
+                        // minute: "numeric",
+                        // timeZone: "Europe/Berlin",
                     }
                 );
                 console.log(
@@ -363,26 +369,252 @@ const Usecase2 = {
         },
     },
     withPackage: {
-        // TODO: Common formats
+        // Common formats
         commonFormats: {
-            ISO8601: () => {},
-            American: () => {},
-            European: () => {},
-            WrittenOut: () => {},
-            UnixTimestamp: () => {},
-            "24Hours": () => {},
-            RFC2822: () => {},
-            LongDate: () => {},
+            ISO8601: () => {
+                console.log("\n\tWith Package - Common Format - [ISO8601]\n");
+                const time = dayjs(dateFromBackend).toISOString();
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to ISO8601: ",
+                    time
+                );
+                return time;
+            },
+            American: () => {
+                console.log(
+                    "\n\tWith Package - American Format - [MM/DD/YYYY or MM/DD/YY]\n"
+                );
+                const format1 = dayjs(dateFromBackend).format("MM/DD/YYYY");
+                const format2 = dayjs(dateFromBackend).format("MM/DD/YY");
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to American Format: ",
+                    format1,
+                    " or ",
+                    format2
+                );
+                return {
+                    format1,
+                    format2,
+                };
+            },
+            European: () => {
+                console.log(
+                    "\n\tWith Package - European Format - [DD/MM/YYYY or DD/MM/YY]\n"
+                );
+                const format1 = dayjs(dateFromBackend).format("DD/MM/YYYY");
+                const format2 = dayjs(dateFromBackend).format("DD/MM/YY");
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to European Format: ",
+                    format1,
+                    " or ",
+                    format2
+                );
+                return {
+                    format1,
+                    format2,
+                };
+            },
+            WrittenOut: () => {
+                console.log(
+                    "\n\tWith Package - Written Out Format - [Month DD, YYYY]\n"
+                );
+                const result = dayjs(dateFromBackend).format("MMMM DD, YYYY");
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to Written Out Format: ",
+                    result
+                );
+                return result;
+            },
+            UnixTimestamp: () => {
+                console.log(
+                    "\n\tWith Package - Common Format - [UnixTimestamp]\n"
+                );
+                const time = dayjs(dateFromBackend).unix();
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to UnixTimestamp: ",
+                    time
+                );
+                return time;
+            },
+            "24Hours": () => {
+                console.log("\n\tWith Package - 24Hours Format - [HH:MM:SS]\n");
+                const time = dayjs(dateFromBackend).format("HH:MM:SS");
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to 24Hours format: ",
+                    time
+                );
+                return time;
+            },
+            RFC2822: () => {
+                console.log(
+                    "\n\tWith Package - RFC2822 Format - [Day, DD Mon YYYY HH:MM:SS +ZZZZ]\n"
+                );
+                const time = dayjs(dateFromBackend).format(
+                    "ddd, DD MMM YYYY HH:mm:ss ZZ"
+                );
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to RFC2822 format: ",
+                    time
+                );
+                return time;
+            },
+            LongDate: () => {
+                console.log(
+                    "\n\tWith Package - Long Date Format - [Day, Month DD, YYYY]\n"
+                );
+                const time = dayjs(dateFromBackend).format("ddd, MMM DD, YYYY");
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to RFC2822 format: ",
+                    time
+                );
+                return time;
+            },
         },
-        // TODO: Based on nationalities
+        // Based on nationalities
         nationalities: {
-            UnitedStates: () => {},
-            UnitedKingdom: () => {},
-            Canada: () => {},
-            Australia: () => {},
-            Japan: () => {},
-            China: () => {},
-            Germany: () => {},
+            UnitedStates: () => {
+                console.log(
+                    "\n\tWith Package - United States Format - [MM/DD/YYYY or MM/DD/YY]\n"
+                );
+                const format1 = dayjs(dateFromBackend)
+                    .tz("America/New_York")
+                    .format("MM/DD/YYYY");
+                const format2 = dayjs(dateFromBackend)
+                    .tz("America/New_York")
+                    .format("MM/DD/YY");
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to United States Format: ",
+                    format1,
+                    " or ",
+                    format2
+                );
+                return {
+                    format1,
+                    format2,
+                };
+            },
+            UnitedKingdom: () => {
+                console.log(
+                    "\n\tWith Package - United Kingdom Format - [DD/MM/YYYY or DD/MM/YY]\n"
+                );
+                const format1 = dayjs(dateFromBackend).format("DD/MM/YYYY");
+                const format2 = dayjs(dateFromBackend).format("DD/MM/YY");
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to United Kingdom Format: ",
+                    format1,
+                    " or ",
+                    format2
+                );
+                return {
+                    format1,
+                    format2,
+                };
+            },
+            Canada: () => {
+                console.log(
+                    "\n\tWith Package - Canada Format - [MM/DD/YYYY or DD/MM/YYYY]\n"
+                );
+                const format1 = dayjs(dateFromBackend).format("MM/DD/YYYY");
+                const format2 = dayjs(dateFromBackend).format("DD/MM/YYYY");
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to Canada Format: ",
+                    format1,
+                    " or ",
+                    format2
+                );
+                return {
+                    format1,
+                    format2,
+                };
+            },
+            Australia: () => {
+                console.log(
+                    "\n\tWith Package - Australia Format - [DD/MM/YYYY or DD/MM/YY]\n"
+                );
+                const format1 = dayjs(dateFromBackend).format("DD/MM/YYYY");
+                const format2 = dayjs(dateFromBackend).format("DD/MM/YY");
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to Australia Format: ",
+                    format1,
+                    " or ",
+                    format2
+                );
+                return {
+                    format1,
+                    format2,
+                };
+            },
+            Japan: () => {
+                console.log(
+                    "\n\tWith Package - Japan Format - [YYYY年MM月DD日]\n"
+                );
+                const time =
+                    dayjs(dateFromBackend).format("YYYY年MM月DD日 HH:mm");
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to Japan Format: ",
+                    time
+                );
+                return {
+                    time,
+                };
+            },
+            China: () => {
+                console.log(
+                    "\n\tWith Package - China Format - [YYYY年MM月DD日]\n"
+                );
+                const time =
+                    dayjs(dateFromBackend).format("YYYY年MM月DD日 HH:mm");
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to China Format: ",
+                    time
+                );
+                return {
+                    time,
+                };
+            },
+            Germany: () => {
+                console.log(
+                    "\n\tWith Package - Germany Format - [DD.MM.YYYY]\n"
+                );
+                const time = dayjs(dateFromBackend).format("DD.MM.YYYY");
+                console.log(
+                    "Original date: ",
+                    dateFromBackend,
+                    " to Germany Format: ",
+                    time
+                );
+                return {
+                    time,
+                };
+            },
         },
     },
 };
